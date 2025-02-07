a1aa7c3b056c56c083293df89d4f10e94124991b
kunj-09 <kunj.mm@gmail.com>
2025-02-08 02:02:46 +0530

    commit

--- front/src/routes/chatPage/ChatPage.jsx
+++ front/src/routes/chatPage/ChatPage.jsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 import Markdown from "react-markdown";
 import { IKImage } from "imagekitio-react";
-
+// hiii 
 const ChatPage = () => {
   const path = useLocation().pathname;
   const chatId = path.split("/").pop();
